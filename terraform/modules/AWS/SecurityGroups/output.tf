0dbf9e80d814bc4471c6e3aeab3501d9bb0b2626
AlexandreGusson <58993792+AlexandreGusson@users.noreply.github.com>
2020-04-01 15:02:26 -0300

    Create output.tf

---
+++ terraform/modules/AWS/SecurityGroups/output.tf
@@ -0,0 +1,10 @@
+output "BastionSecurityGroupID" {
+  value       = aws_security_group.simulator_bastion_sg.id
+  description = "Bastion security group id"
+}
+
+output "ControlPlaneSecurityGroupID" {
+  value       = aws_security_group.simulator_controlplane_sg.id
+  description = "Controlplane (Kubernetes) security group id"
+}
+
